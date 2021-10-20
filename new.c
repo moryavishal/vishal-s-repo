d7e2fe574b6d8f84c0ddae8e5e498aa62b0c18aa
Skaronite <72257004+Skaronite@users.noreply.github.com>
2021-10-20 13:52:30 +0530

    Update new.c

--- new.c
+++ new.c
@@ -3,17 +3,18 @@
 int main()
 {
     char name,country,skill;
-    int age;
+    float age;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
     scanf("%c",&name);
     printf("Enter your Country of Residance : ");//Get user country
     scanf("%c",&country);
     printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
+    printf("%f",&age);
     printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
     scanf(" %c",&skill);
-    scanf("Thank you %c Happy Hacking",name);//Thank the user
+    scanf("Thank you %c Happy Hacking",name);
+   //Thank the user
 
     return 0;
 }
